f35ba6cdfa441a115dca4b53b3fc6e6d69847205
IDerkBot <ivanderk2003@gmail.com>
2022-11-19 23:05:29 +0300

    Добавлено окно авторизации

---
+++ GameLibrary/Views/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace GameLibrary.Views.Windows;
+
+public partial class AuthorizationWindow : Window
+{
+    public AuthorizationWindow()
+    {
+        InitializeComponent();
+    }
+}
