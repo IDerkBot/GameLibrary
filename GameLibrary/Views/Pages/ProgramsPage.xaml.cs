55ebf32eeef823db55ad35b237ff4a4e82692bf7
IDerkBot <ivanderk2003@gmail.com>
2022-11-20 17:09:14 +0300

    Добавлена страница программ

---
+++ GameLibrary/Views/Pages/ProgramsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace GameLibrary.Views.Pages;
+
+public partial class ProgramsPage : Page
+{
+    public ProgramsPage()
+    {
+        InitializeComponent();
+    }
+}
