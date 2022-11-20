8bb99f780fda987c85897665e03f10c85a5376ba
IDerkBot <ivanderk2003@gmail.com>
2022-11-20 17:10:25 +0300

    Добавлена страница игр

---
+++ GameLibrary/Views/Pages/GamesPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace GameLibrary.Views.Pages;
+
+public partial class GamesPage : Page
+{
+    public GamesPage()
+    {
+        InitializeComponent();
+    }
+}
