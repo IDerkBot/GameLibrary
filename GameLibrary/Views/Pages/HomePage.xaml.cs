0f9ed3de1cb5fa14b21cc29bfc68e03190f91bb9
IDerkBot <ivanderk2003@gmail.com>
2022-11-20 17:10:10 +0300

    Добавлена домашняя страница

---
+++ GameLibrary/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameLibrary.Views.Pages;
+
+public partial class HomePage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private async void HomePage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var count = Global.Database.Apps.Count();
+        var random = new Random();
+        var apps = new List<Models.Entity.App>();
+
+        for (var i = 0; i < 20; i++)
+        {
+            apps.Add(await Global.Database.Apps.FirstAsync(x => x.Id == random.Next(1, count)));
+        }
+
+        LvApps.ItemsSource = apps;
+    }
+}
