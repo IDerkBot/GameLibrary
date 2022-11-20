f8731c3331fe379618887572858a3f90b9ff011a
IDerkBot <ivanderk2003@gmail.com>
2022-11-20 17:09:06 +0300

    Добавлена страница настроек

---
+++ GameLibrary/Views/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace GameLibrary.Views.Pages;
+
+public partial class SettingsPage : Page
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
